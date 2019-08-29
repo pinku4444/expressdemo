dba51bc4a976d7181ada555f838a1b268a004ce6
pinku4444 <pinku.kumar@successive.tech>
2019-08-29 18:46:53 +0530

    added babelrc

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import config from './config/config'
 import Server from './server';
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+
 
 const server = new Server(config);
 server.bootstrap();
